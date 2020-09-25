d35b17e5990f2756f29c35de1b665b49c0bec619
MirInjamamul <injamul120221@gmail.com>
2020-09-25 21:37:42 +0600

    exam list can be seen in user panel. with history error .(need to solve the bug)

--- geniusEvents/welcome.php
+++ geniusEvents/welcome.php
@@ -64,19 +64,34 @@
             <div class="col-md-12">
                 <?php if(@$_GET['q']==1) 
                 {
-                    $result = mysqli_query($con,"SELECT * FROM quiz ORDER BY date DESC") or die('Error');
-                    echo  '<div class="panel"><div class="table-responsive"><table class="table table-striped title1">
-                    <tr><td><center><b>S.N.</b></center></td><td><center><b>Topic</b></center></td><td><center><b>Total question</b></center></td><td><center><b>Marks</center></b></td><td><center><b>Action</b></center></td></tr>';
+                    $result = mysqli_query($con,"SELECT * FROM event_exam ORDER BY date DESC") or die('Error Quiz table Not Table');
+                    echo  '<div class="panel">
+                                <div class="table-responsive">
+                                    <table class="table table-striped title1">
+                                        <tr>    
+                                            <td><center><b>S.N.</b></center></td>
+                                            <td><center><b>Topic</b></center></td>
+                                            <td><center><b>Total question</b></center></td>
+                                            <td><center><b>Marks</center></b></td>
+                                            <td><center><b>Action</b></center></td>
+                                        </tr>';
                     $c=1;
                     while($row = mysqli_fetch_array($result)) {
                         $title = $row['title'];
                         $total = $row['total'];
-                        $sahi = $row['sahi'];
-                        $eid = $row['eid'];
+                        $sahi = $row['positive_number'];
+                        $eid = $row['exam_id'];
                     $q12=mysqli_query($con,"SELECT score FROM history WHERE eid='$eid' AND email='$email'" )or die('Error98');
                     $rowcount=mysqli_num_rows($q12);	
                     if($rowcount == 0){
-                        echo '<tr><td><center>'.$c++.'</center></td><td><center>'.$title.'</center></td><td><center>'.$total.'</center></td><td><center>'.$sahi*$total.'</center></td><td><center><b><a href="welcome.php?q=quiz&step=2&eid='.$eid.'&n=1&t='.$total.'" class="btn sub1" style="color:black;margin:0px;background:#1de9b6"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Start</b></span></a></b></center></td></tr>';
+                        echo 
+                            '<tr>
+                                <td><center>'.$c++.'</center></td>
+                                <td><center>'.$title.'</center></td>
+                                <td><center>'.$total.'</center></td>
+                                <td><center>'.$sahi*$total.'</center></td>
+                                <td><center><b><a href="welcome.php?q=quiz&step=2&eid='.$eid.'&n=1&t='.$total.'" class="btn sub1" style="color:black;margin:0px;background:#1de9b6"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Start</b></span></a></b></center></td>
+                            </tr>';
                     }
                     else
                     {
