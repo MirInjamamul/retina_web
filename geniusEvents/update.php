02488d9457d939a8360bf220752e9666cfc2502e
MirInjamamul <injamul120221@gmail.com>
2020-09-28 14:41:00 +0600

    admin can see the registered user and delete the all history of that user

--- geniusEvents/update.php
+++ geniusEvents/update.php
@@ -3,16 +3,15 @@
   session_start();
   $email=$_SESSION['email'];
 
-  if(isset($_SESSION['key']))
+  if(@$_GET['demail']) 
   {
-    if(@$_GET['demail'] && $_SESSION['key']=='suryapinky') 
-    {
+    
       $demail=@$_GET['demail'];
-      $r1 = mysqli_query($con,"DELETE FROM rank WHERE email='$demail' ") or die('Error');
-      $r2 = mysqli_query($con,"DELETE FROM history WHERE email='$demail' ") or die('Error');
-      $result = mysqli_query($con,"DELETE FROM user WHERE email='$demail' ") or die('Error');
+      $r1 = mysqli_query($con,"DELETE FROM event_exam_rank WHERE email='$demail' ") or die('Error');
+      $r2 = mysqli_query($con,"DELETE FROM event_exam_history WHERE email='$demail' ") or die('Error');
+      $result = mysqli_query($con,"DELETE FROM event_user WHERE email='$demail' ") or die('Error');
       header("location:dashboard.php?q=1");
-    }
+    
   }
 
   if(@$_GET['q']== 'rmquiz')
